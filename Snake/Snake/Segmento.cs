33dcc627ac1c231f234316c58f401381d1b02a55
Alex Deb Duq <alejandrodebduq@gmail.com>
2023-04-25 18:27:53 +0200

    Rellenado segmento
    Segmento inicial

--- Snake/Snake/Segmento.cs
+++ Snake/Snake/Segmento.cs
@@ -4,16 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Snake
 {
     internal class Segmento
     {
         private PictureBox picBox = new PictureBox();
-        
-        public Segmento()
+        private int x;
+        private int y;
+        private int size;
+        private Color color;
+        private string direccion;
+        public Segmento(int x, int y, int size, Color color, string direccion)
         {
-            //picBox.Location();
+            picBox.Location = new Point(x, y);
+            this.x = x;
+            this.y = y;
+            this.size = size;
+            this.color = color;
+            this.direccion = direccion;
         }
     }
 }
